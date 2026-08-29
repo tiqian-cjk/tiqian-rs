29fd8374983c26d351698e519b783cb37d5ecc30
Icemic <bingfeng.web@gmail.com>
2026-08-29 16:11:52 +0800

    chore: adjust folder structure of tests

--- tests/org/tiqian/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod clreq;
-pub mod core;
-pub mod font;
-pub mod layout;
-pub mod linebreak;
-pub mod shaping;
--- tests/tiqian.rs
+++ tests/tiqian.rs
@@ -1,1 +1,6 @@
-mod org;
+mod clreq;
+mod core;
+mod font;
+mod layout;
+mod linebreak;
+mod shaping;
